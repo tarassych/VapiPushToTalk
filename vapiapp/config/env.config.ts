d715002a2329f0668e838d7deb4768a12930935f
tarassych <taras.sych@gmail.com>
2024-04-22 21:54:44 +0300

    initial app commit

---
+++ vapiapp/config/env.config.ts
@@ -0,0 +1,18 @@
+export const envConfig = {
+  vapi: {
+    apiUrl: process.env.NEXT_PUBLIC_VAPI_API_URL ?? "https://api.vapi.ai",
+    token: process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN ?? "1ec9d220-ea8e-4df7-b82c-967f77c59d4e",
+  },
+};
+
+
+
+// API Key (use this Key when dealing with Vapi via Backend Endpoints): 993e0c57-a509-4cd4-8617-dfd31a9084a8
+// Public Key (Use this Key when using Vapi Client SDKs in Frontend.): 1ec9d220-ea8e-4df7-b82c-967f77c59d4e
+// Assistant ID: 90e77507-4c28-4cfe-89c9-79f312cde13a
+
+
+// use this username/pw to log into Vapi
+// 4:29
+// username: yisroel@ringiq.ai
+// pw: ijIJyjTH16%^4523RGts
