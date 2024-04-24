985c2dc34a9c4c6d1fa424b41619e4fac2e75b57
tarassych <taras.sych@gmail.com>
2024-04-24 15:02:38 +0300

    button styles updated

--- vapiapp/components/app/assistantButton.tsx
+++ vapiapp/components/app/assistantButton.tsx
@@ -1,57 +1,62 @@
-import { CALL_STATUS, useVapi } from "@/hooks/useVapi";
-import { Loader2, Mic, Square } from "lucide-react";
-import { Button } from "../ui/button";
+import {CALL_STATUS, useVapi} from "@/hooks/useVapi";
+import {Loader2, Mic, Square} from "lucide-react";
+import {Button} from "../ui/button";
 
 const AssistantButton = ({
-  toggleCall,
-  callStatus,
-  audioLevel = 0,
-}: Partial<ReturnType<typeof useVapi>>) => {
-  const color =
-    callStatus === CALL_STATUS.ACTIVE
-      ? "red"
-      : callStatus === CALL_STATUS.LOADING
-      ? "orange"
-      : "green";
-  const buttonStyle = {
-    borderRadius: "50%",
-    width: "50px",
-    height: "50px",
-    color: "white",
-    border: "none",
-    boxShadow: `1px 1px ${10 + audioLevel * 40}px ${
-      audioLevel * 10
-    }px ${color}`,
-    backgroundColor:
-      callStatus === CALL_STATUS.ACTIVE
-        ? "red"
-        : callStatus === CALL_STATUS.LOADING
-        ? "orange"
-        : "green",
-    cursor: "pointer",
-  };
-
-  return (
-    <Button
-      style={buttonStyle}
-      className={`transition ease-in-out ${
+                             toggleCall,
+                             callStatus,
+                             audioLevel = 0,
+                         }: Partial<ReturnType<typeof useVapi>>) => {
+    const color =
         callStatus === CALL_STATUS.ACTIVE
-          ? "bg-red-500 hover:bg-red-700"
-          : callStatus === CALL_STATUS.LOADING
-          ? "bg-orange-500 hover:bg-orange-700"
-          : "bg-green-500 hover:bg-green-700"
-      } flex items-center justify-center`}
-      onClick={toggleCall}
-    >
-      {callStatus === CALL_STATUS.ACTIVE ? (
-        <Square />
-      ) : callStatus === CALL_STATUS.LOADING ? (
-        <Loader2 className="animate-spin" />
-      ) : (
-        <Mic />
-      )}
-    </Button>
-  );
+            ? "#E16269"
+            : callStatus === CALL_STATUS.LOADING
+                ? "#F2B5A0"
+                : "#A5D0A3";
+    const iconStyle = {
+      width: "4em",
+      height: "4em",
+    };
+
+    const buttonStyle = {
+        borderRadius: "50%",
+        width: "15em",
+        height: "15em",
+        color: "white",
+        border: "none",
+        boxShadow: `1px 1px ${10 + audioLevel * 40}px ${
+            audioLevel * 10
+        }px ${color}`,
+        backgroundColor:
+            callStatus === CALL_STATUS.ACTIVE
+                ? "#E16269"
+                : callStatus === CALL_STATUS.LOADING
+                    ? "#F2B5A0"
+                    : "#A5D0A3",
+        cursor: "pointer",
+    };
+
+    return (
+        <Button
+            style={buttonStyle}
+            className={`transition ease-in-out ${
+                callStatus === CALL_STATUS.ACTIVE
+                    ? "bg-red-500 hover:bg-red-700"
+                    : callStatus === CALL_STATUS.LOADING
+                        ? "bg-orange-500 hover:bg-orange-700"
+                        : "bg-green-500 hover:bg-green-700"
+            } flex items-center justify-center`}
+            onClick={toggleCall}
+        >
+            {callStatus === CALL_STATUS.ACTIVE ? (
+                <Square style={iconStyle}/>
+            ) : callStatus === CALL_STATUS.LOADING ? (
+                <Loader2 style={iconStyle} className="animate-spin"/>
+            ) : (
+                <Mic style={iconStyle}/>
+            )}
+        </Button>
+    );
 };
 
-export { AssistantButton };
+export {AssistantButton};
--- vapiapp/pages/index.tsx
+++ vapiapp/pages/index.tsx
@@ -7,20 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-12 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-12 ${inter.className}`}
     >
-      <div className="text-center">
-        <h1 className="text-3xl">Welcome RingIQ Ai Call Attendant</h1>
-        <p className="text-slate-600">
-          Press call button to make a VoIP cal to a test RingIQ Ai-Call Attendant.
-        </p>
-        <p className="text-slate-600">
-          The RingIQ Ai operator will know exam times, insurance information, and be available 24/7.
-        </p>
-        <p className="text-slate-600">
-          You can book an appointment with this test location.
-        </p>
-      </div>
       <Assistant/>
     </main>
   );
